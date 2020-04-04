d0f420fcac21934f7c91fe7f1f5d78c6d2a1c22f
ramishenouda <ramishenoouda@outlook.com>
2020-04-04 19:30:24 +0200

    added ngx-bootstrap for dropdown menu, bootswatch for styling and alertifyJS for noficatinos

--- DatingApp-SPA/src/app/app.component.ts
+++ DatingApp-SPA/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './_services/Auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit{
+  constructor(private authService: AuthService) { }
+  ngOnInit(): void {
+    const token = localStorage.getItem('token');
+    if(token) {
+      this.authService.setToken(token);
+    }
+  }
 }
--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
-import { FormsModule } from '@angular/forms';
 import { AuthService } from './_services/Auth.service';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
@@ -17,7 +19,7 @@ import { ErrorInterceptorProvider } from './_services/error.interceptor';
     HomeComponent,
     RegisterComponent
   ],
-  imports: [BrowserModule, HttpClientModule, FormsModule],
+  imports: [BrowserModule, HttpClientModule, FormsModule, BrowserAnimationsModule, BsDropdownModule.forRoot()],
   providers: [AuthService, ErrorInterceptorProvider],
   bootstrap: [AppComponent]
 })
